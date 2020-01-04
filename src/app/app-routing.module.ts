7f1d55531eb8b072b411b215ca35788ea60a6dc7
Adeleke Oladapo <adelekeoladapo@gmail.com>
2020-01-04 02:30:25 +0100

    Completed list and detail pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SignInComponent} from './pages/sign-in/sign-in.component';
+import {HomeComponent} from './pages/home/home.component';
+import {SignUpComponent} from './pages/sign-up/sign-up.component';
+import {MovieComponent} from './pages/movie/movie.component';
 
 const routes: Routes = [
   {
     path: 'sign-in',
     component: SignInComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  },
+  {
+    path: 'list-of-movies',
+    component: HomeComponent
+  },
+  {
+    path: 'movie',
+    component: MovieComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { MovieComponent } from './pages/movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    HomeComponent,
+    SignUpComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
